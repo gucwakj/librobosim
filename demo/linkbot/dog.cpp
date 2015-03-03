79d1cfdf79ce1077969b30009c0d52329caa9cb7
Kevin Gucwa <gucwakj@gmail.com>
2015-03-03 13:54:04 -0800

    doggie drawing code

---
+++ demo/linkbot/dog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "clinkbot.hpp"
+
+int main(int argc, char *argv[]) {
+	CLinkbotI robot1, robot2/*, robot3, robot4, robot5, robot6*/;
+	robot1.connect();
+	robot2.connect();
+	/*robot3.connect();
+	robot4.connect();
+	robot5.connect();
+	robot6.connect();*/
+
+	return 0;
+}
