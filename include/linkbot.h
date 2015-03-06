3e5520d91452b131c608d77464d6f277cb7cc4bc
Kevin Gucwa <gucwakj@gmail.com>
2015-03-06 10:53:31 -0800

    add accel functions from librs

--- include/linkbot.h
+++ include/linkbot.h
@@ -11,7 +11,21 @@ class CLinkbot : public rsSim::Linkbot {
 		CLinkbot(void);
 		virtual ~CLinkbot(void);
 
+		int accelJointAngleNB(rsLinkbot::Joint, double, double);
+		int accelJointCycloidalNB(rsLinkbot::Joint, double, double);
+		int accelJointHarmonicNB(rsLinkbot::Joint, double, double);
+		int accelJointSmoothNB(rsLinkbot::Joint, double, double, double, double);
+		int accelJointTimeNB(rsLinkbot::Joint, double, double);
+		int accelJointToMaxSpeedNB(rsLinkbot::Joint, double);
+		int accelJointToVelocityNB(rsLinkbot::Joint, double, double);
 		int connect(char* = NULL, int = 3);
+		int driveAccelCycloidalNB(double, double, double);
+		int driveAccelDistanceNB(double, double, double);
+		int driveAccelHarmonicNB(double, double, double);
+		int driveAccelSmoothNB(double, double, double, double, double);
+		int driveAccelTimeNB(double, double, double);
+		int driveAccelToMaxSpeedNB(double, double);
+		int driveAccelToVelocityNB(double, double, double);
 		int turnLeft(double, double, double);
 		int turnRight(double, double, double);
 };
