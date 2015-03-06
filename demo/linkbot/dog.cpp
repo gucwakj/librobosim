0dddc4a815f75bfab2b4d6b8ff41f8081b2dfea4
Kevin Gucwa <gucwakj@gmail.com>
2015-03-05 17:27:50 -0800

    added linkbot dog demo

--- demo/linkbot/dog.cpp
+++ demo/linkbot/dog.cpp
@@ -2,13 +2,33 @@
 #include "clinkbot.hpp"
 
 int main(int argc, char *argv[]) {
-	CLinkbotI robot1, robot2/*, robot3, robot4, robot5, robot6*/;
+	CLinkbotI robot1, robot2, robot3, robot4;		// trunk
+	CLinkbotI robot5, robot6, robot7, robot8;		// leg
+	CLinkbotI robot9; CLinkbotL robot10; CLinkbotI robot11, robot12, robot13;	// leg
+	CLinkbotI robot14, robot15, robot16, robot17;	// leg
+	CLinkbotI robot18, robot19, robot20, robot21, robot22;	// leg
 	robot1.connect();
 	robot2.connect();
-	/*robot3.connect();
+	robot3.connect();
 	robot4.connect();
 	robot5.connect();
-	robot6.connect();*/
+	robot6.connect();
+	robot7.connect();
+	robot8.connect();
+	robot9.connect();
+	robot10.connect();
+	robot11.connect();
+	robot12.connect();
+	robot13.connect();
+	robot14.connect();
+	robot15.connect();
+	robot16.connect();
+	robot17.connect();
+	robot18.connect();
+	robot19.connect();
+	robot20.connect();
+	robot21.connect();
+	robot22.connect();
 
 	return 0;
 }
