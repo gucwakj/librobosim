a9a349350e8f8fb9428cdac2261877302c06006d
Kevin Gucwa <gucwakj@gmail.com>
2015-05-12 09:26:21 -0700

    create sinusoid demo to visualize research motions

--- demo/linkbot/one.cpp
+++ demo/linkbot/one.cpp
@@ -2,7 +2,22 @@
 #include "linkbot.h"
 
 int main(int argc, char *argv[]) {
-	CLinkbotI robot;
+	CLinkbotI robot1;//, robot2;
+
+	g_sim->setGoalSinusoid(1, 1, 0);
+
+	robot1.setSinusoidGain(0.988463);
+	robot1.setSinusoidFrequency(0.991112);
+	robot1.setSinusoidPhaseShift(0.009770);
+	robot1.moveJointSinusoid(rsLinkbot::JOINT3);
+
+	/*robot2.setSinusoidGain(0.343820);
+	robot2.setSinusoidFrequency(0.977132);
+	robot2.setSinusoidPhaseShift(0);
+	robot2.moveJointSinusoid(rsLinkbot::JOINT3);*/
+
+	robot1.delaySeconds(10);
+	std::cerr << "fitness: " << g_sim->getFitness() << std::endl;
 
 	return 0;
 }
