c05936fc161a084837ea02cffa5a0249a5fab160
Kevin Gucwa <gucwakj@gmail.com>
2015-12-02 15:34:53 -0800

    move mindstorms forward with joints 2+3

--- src/mindstorms.cpp
+++ src/mindstorms.cpp
@@ -7,7 +7,7 @@ CMindstorms::CMindstorms(const char *name, bool pause) :	rsRobots::Robot(rs::EV3
 															rsScene::Mindstorms(rs::EV3),
 															rsSim::Mindstorms(),
 															rsCallback::Mindstorms(),
-															roboSim::Robot(Bodies::Joint1, Bodies::Joint2) {
+															roboSim::Robot(Bodies::Joint2, Bodies::Joint3) {
 	// create simulation object if necessary
 	if (g_sim == NULL) g_sim = new RoboSim(name, pause);
 
@@ -21,8 +21,8 @@ CMindstorms::~CMindstorms(void) {
 
 int CMindstorms::getJointAngles(double &angle1, double &angle2, double &angle3, double &angle4, int numReadings) {
 	angle1 = 0;
-	this->getJointAngle(Bodies::Joint1, angle2, numReadings);
-	this->getJointAngle(Bodies::Joint2, angle3, numReadings);
+	this->getJointAngle(Bodies::Joint2, angle2, numReadings);
+	this->getJointAngle(Bodies::Joint3, angle3, numReadings);
 	angle4 = 0;
 
 	// success
@@ -31,8 +31,8 @@ int CMindstorms::getJointAngles(double &angle1, double &angle2, double &angle3, 
 
 int CMindstorms::getJointAnglesInstant(double &angle1, double &angle2, double &angle3, double &angle4) {
 	angle1 = 0;
-	this->getJointAngleInstant(Bodies::Joint1, angle2);
-	this->getJointAngleInstant(Bodies::Joint2, angle3);
+	this->getJointAngleInstant(Bodies::Joint2, angle2);
+	this->getJointAngleInstant(Bodies::Joint3, angle3);
 	angle4 = 0;
 
 	// success
@@ -41,8 +41,8 @@ int CMindstorms::getJointAnglesInstant(double &angle1, double &angle2, double &a
 
 int CMindstorms::getJointSpeeds(double &speed1, double &speed2, double &speed3, double &speed4) {
 	speed1 = 0;
-	this->getJointSpeed(Bodies::Joint1, speed2);
-	this->getJointSpeed(Bodies::Joint2, speed3);
+	this->getJointSpeed(Bodies::Joint2, speed2);
+	this->getJointSpeed(Bodies::Joint3, speed3);
 	speed4 = 0;
 
 	// success
@@ -51,8 +51,8 @@ int CMindstorms::getJointSpeeds(double &speed1, double &speed2, double &speed3, 
 
 int CMindstorms::getJointSpeedRatios(double &ratio1, double &ratio2, double &ratio3, double &ratio4) {
 	ratio1 = 0;
-	this->getJointSpeedRatio(Bodies::Joint1, ratio2);
-	this->getJointSpeedRatio(Bodies::Joint2, ratio3);
+	this->getJointSpeedRatio(Bodies::Joint2, ratio2);
+	this->getJointSpeedRatio(Bodies::Joint3, ratio3);
 	ratio4 = 0;
 
 	// success
@@ -68,8 +68,8 @@ int CMindstorms::move(double angle1, double angle2, double angle3, double angle4
 }
 
 int CMindstorms::moveNB(double angle1, double angle2, double angle3, double angle4) {
-	this->moveJointNB(Bodies::Joint1, angle2);
-	this->moveJointNB(Bodies::Joint2, angle3);
+	this->moveJointNB(Bodies::Joint2, angle2);
+	this->moveJointNB(Bodies::Joint3, angle3);
 
 	// success
 	return 0;
@@ -84,8 +84,8 @@ int CMindstorms::moveTo(double angle1, double angle2, double angle3, double angl
 }
 
 int CMindstorms::moveToNB(double angle1, double angle2, double angle3, double angle4) {
-	this->moveJointToNB(Bodies::Joint1, angle2);
-	this->moveJointToNB(Bodies::Joint2, angle3);
+	this->moveJointToNB(Bodies::Joint2, angle2);
+	this->moveJointToNB(Bodies::Joint3, angle3);
 
 	// success
 	return 0;
@@ -116,24 +116,24 @@ int CMindstorms::recordAnglesBegin(robotRecordData_t &time, robotRecordData_t &a
 	double **angles = new double * [_dof];
 	angle1 = new double[RECORD_ANGLE_ALLOC_SIZE];
 	angle2 = new double[RECORD_ANGLE_ALLOC_SIZE];
-	angles[Bodies::Joint1] = angle1;
-	angles[Bodies::Joint2] = angle2;
+	angles[Bodies::Joint2] = angle1;
+	angles[Bodies::Joint3] = angle2;
 
 	// call base class recording function
 	return roboSim::Robot::recordAnglesBegin(time, angles, seconds, shiftData);
 }
 
 int CMindstorms::setJointSpeeds(double speed1, double speed2, double speed3, double speed4) {
-	this->setJointSpeed(Bodies::Joint1, speed2);
-	this->setJointSpeed(Bodies::Joint2, speed3);
+	this->setJointSpeed(Bodies::Joint2, speed2);
+	this->setJointSpeed(Bodies::Joint3, speed3);
 
 	// success
 	return 0;
 }
 
 int CMindstorms::setJointSpeedRatios(double ratio1, double ratio2, double ratio3, double ratio4) {
-	this->setJointSpeedRatio(Bodies::Joint1, ratio2);
-	this->setJointSpeedRatio(Bodies::Joint2, ratio3);
+	this->setJointSpeedRatio(Bodies::Joint2, ratio2);
+	this->setJointSpeedRatio(Bodies::Joint3, ratio3);
 
 	// success
 	return 0;
